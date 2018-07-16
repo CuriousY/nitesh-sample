0cc6290723a04a71e3127a7d0571d5aeb04f85cc
Nitesh <nickscorpio4u@gmail.com>
2018-07-16 20:32:57 +0530

    featured link pojo

--- core/src/main/java/org/sample/pojo/ContentHeroPojo.java
+++ core/src/main/java/org/sample/pojo/ContentHeroPojo.java
@@ -3,8 +3,11 @@ package org.sample.pojo;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.commons.json.JSONArray;
+import org.apache.sling.commons.json.JSONObject;
 import org.demo.service.IGreeter;
 
 import com.adobe.cq.sightly.WCMUsePojo;
@@ -19,10 +22,16 @@ public class ContentHeroPojo extends WCMUsePojo {
 
 	private String heroImagePath;
 
+	private String strFeatureList;
+
 	private List<HashMap> featureList;
 
 	public String getHeroLink() {
 		return heroLink;
+	}
+
+	public String getStrFeatureList() {
+		return strFeatureList;
 	}
 
 	public void setHeroLink(String heroLink) {
@@ -53,6 +62,15 @@ public class ContentHeroPojo extends WCMUsePojo {
 			heroLink = map.get("ctaLink", String.class);
 			heroDescription = map.get("description", String.class);
 			heroImagePath = map.get("imagePath", String.class);
+			String[] test = map.get("featureItemList", String[].class);
+			JSONObject obj = null;
+			if (test.length > 0) {
+				for (int i = 0; i < test.length; i++) {
+					obj = new JSONObject(test[i]);
+				}
+			}
+			strFeatureList = obj.getString("ctaLink");
+
 		}
 
 	}
